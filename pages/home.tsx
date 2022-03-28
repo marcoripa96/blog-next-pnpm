ae04cdea8dff62f3c20d734d6b1c6c0e75eecf2d
riccardomerlo <merloriki97@live.it>
2022-03-29 00:50:12 +0200

    add posts to home

--- pages/home.tsx
+++ pages/home.tsx
@@ -3,25 +3,29 @@ import path from 'path'
 import Link from 'next/link'
 import Image from 'next/image'
 
+import styles from 'styles/home.module.css'
+
+import clsx from 'clsx'
+
 const Home = ({ posts }: any) => {
   return (
     <div className="mt-5">
-      {/* {posts.map((post: any, index: any) => (
-        <Link href={'/blog/' + post.slug} passHref key={index}>
-          <div className="card mb-3 pointer" style={{ maxWidth: '540px' }}>
+      {posts.map((post: any, index: any) => (
+        <Link href={'/posts/' + post.slug} passHref key={index}>
+          <div className={clsx(styles.card, 'mb-3', 'pointer')} style={{ maxWidth: '540px' }}>
             <div className="row g-0">
               <div className="col-md-8">
                 <div className="card-body">
-                  <h5 className="card-title">{post.frontMatter.title}</h5>
-                  <p className="card-text">{post.frontMatter.description}</p>
+                  <h5 className="card-title">{post.title}</h5>
+                  <p className="card-text">{post.description}</p>
                   <p className="card-text">
-                    <small className="text-muted">{post.frontMatter.date}</small>
+                    <small className="text-muted">{post.date}</small>
                   </p>
                 </div>
               </div>
               <div className="col-md-4 m-auto">
                 <Image
-                  src={post.frontMatter.thumbnailUrl}
+                  src={post.thumbnailUrl}
                   className="img-fluid mt-1 rounded-start"
                   alt="thumbnail"
                   width={500}
@@ -32,7 +36,7 @@ const Home = ({ posts }: any) => {
             </div>
           </div>
         </Link>
-      ))} */}
+      ))}
     </div>
   )
 }
@@ -41,23 +45,23 @@ export const getStaticProps = async () => {
   const files = fs.readdirSync(path.join('pages/posts'))
   
   
-  const posts = files.map(async filename => {
+  const posts = await Promise.all(files.map(async (filename: string) => {
 
     const post = await import('pages/posts/' + filename)
     //const post = await import(path.join('pages/posts', filename));
-    console.log(post.meta)
     //const { data: frontMatter } = matter(markdownWithMeta)
     //console.log(frontMatter)
     return {
-      //frontMatter,
+      ...post.meta,
       slug: filename.split('.')[0]
     }
-  })
+  }));
 
 
+  console.log(posts)
   return {
     props: {
-      //posts
+      posts
     }
   }
 }
